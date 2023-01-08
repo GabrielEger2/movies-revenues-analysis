0f275af57f73c3b419d025fc244ab435ece81f80
Gabriel Eger <gabriel_eger01@hotmail.com>
2023-01-08 12:17:44 -0300

    Collecting movies data
    Collecting movies data with the mix of API and web scraping, and creating a dataframe with pandas with this data.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
 
 # Using TMDB to find a movie director's id inside the API
-api_key = "API KEY"
+api_key = "c36f9d4a781179b2302784e7119c481f"
 director = input('Please say the name of a movie director: ').replace(' ', '%20') #asking the director from an input
 api_path = f'https://api.themoviedb.org/3/search/person?api_key={api_key}&language=en-US&query={director}&page=1&include_adult=false'
 
@@ -20,6 +22,48 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 tables = soup.find_all('table', class_ = 'card credits')
 table = tables[0]
-movies = table.find_all('a', class_ = 'tooltip')
+movies = table.find_all('a', class_ = 'tooltip') # Finding all movies within the direct html table
+
+# Creating the lists that are  going to be used in pandas
+total_titles = []
+total_dates = []
+total_budgets = []
+total_revenues = []
+
+# Finding movies ids with the API and finding it's data with web scrapping
 for movie in movies:
-    movies_collected.append(movie.getText()) # Appending all movies to the list
+    try:
+        movie_name = movie.getText().replace(' ', '%20')
+        api_path = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US&query={movie_name}&page=1&include_adult=false'
+        response = requests.get(url=api_path)
+        data = response.json()
+        data = data['results'][0]
+        url = f"https://www.themoviedb.org/movie/{data['id']}-{data['title'].replace(' ', '-').lower()}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        left_column = soup.find_all('section', class_ = 'facts left_column')
+        for section in left_column: # Getting the important data within the html left column
+            budget = section.contents[7]
+            budget = str(budget.text).replace('Budget ', '')
+            revenue = section.contents[9]
+            revenue = str(revenue.text).replace('Revenue ', '')
+            title = data['title']
+            release_date = data['release_date']
+
+            # Appending the movie information to the lists
+            total_titles.append(title)
+            total_dates.append(release_date)
+            total_budgets.append(budget)
+            total_revenues.append(revenue)
+    except:
+        pass
+
+# creating the dataframe with the lists
+df = pd.DataFrame({
+    "Title": total_titles,
+    "Release Date": total_dates,
+    "Budget": total_budgets,
+    "Revenue": total_revenues
+})
+
+print(df)
