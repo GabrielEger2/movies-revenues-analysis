da245299d96b98485a09d3ce2dbb699d0c9b1159
Gabriel Eger <gabriel_eger01@hotmail.com>
2023-01-07 22:59:24 -0300

    Getting all movies directed by an input director
    I couldn't find a better way to do it, I've tried to find the id of the director with web scrapping but I was not capable to, I also tried to find all movies directed by the inputted director trough the API, but I was also not able to. I'm gonna try web scrap everything tho in the future.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Using TMDB to find a movie director's id inside the API
+api_key = "API KEY"
+director = input('Please say the name of a movie director: ').replace(' ', '%20') #asking the director from an input
+api_path = f'https://api.themoviedb.org/3/search/person?api_key={api_key}&language=en-US&query={director}&page=1&include_adult=false'
 
+# Getting the director's id from a json file
+response = requests.get(url=api_path)
+data = response.json()
+data = data['results'][0]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Creating a list for the movies that the director made
+# I've tried to find a method within the API to find all the movies directed by the director, but I couldn't
+# So I just web scrapped the main page
+movies_collected = []
+url = f"https://www.themoviedb.org/person/{data['id']}-{data['name'].replace(' ', '-').lower()}"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+tables = soup.find_all('table', class_ = 'card credits')
+table = tables[0]
+movies = table.find_all('a', class_ = 'tooltip')
+for movie in movies:
+    movies_collected.append(movie.getText()) # Appending all movies to the list
